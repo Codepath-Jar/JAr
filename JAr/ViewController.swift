aa76d05303b3934037fca981a6960da456beba2d
Abdiaziz Weheliye <aweheliy@ucsd.edu>
2020-04-29 15:27:44 -0700

    Initial Commit

---
+++ JAr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JAr
+//
+//  Created by Abdiaziz Weheliye on 4/29/20.
+//  Copyright © 2020 JAr coorprate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
