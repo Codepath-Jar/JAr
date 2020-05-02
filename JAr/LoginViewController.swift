5b1e8e582443c2559270ff9ca53d0cf5f64270a2
Abdiaziz Weheliye <aweheliy@ucsd.edu>
2020-05-02 15:55:23 -0700

    Added Login and Signup Functionality

--- JAr/LoginViewController.swift
+++ JAr/LoginViewController.swift
@@ -17,7 +17,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view. YEs
     }
     
     @IBAction func onSignIn(_ sender: Any) {
