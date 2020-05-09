fb3c5f2825fc96463f6539f2481de220be6310b9
Abdiaziz Weheliye <aweheliy@ucsd.edu>
2020-05-09 00:30:48 -0700

    done with logout functionality

---
+++ JAr/SettingsViewController.swift
@@ -0,0 +1,40 @@
+//  SettingsViewController.swift
+//  JAr
+//
+//  Created by Abdiaziz Weheliye on 5/8/20.
+//  Copyright © 2020 JAr coorprate. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SettingsViewController: UIViewController {
+
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        let sceneDelegate = self.view.window?.windowScene?.delegate as! SceneDelegate
+        sceneDelegate.window?.rootViewController = loginViewController
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
